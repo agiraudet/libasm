4355c93b620909b78b77c1fa3ce05e7699acda55
agiraude <agiraude@student.42.fr>
2021-02-03 17:53:06 +0100

    norm ok

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 11:42:21 by user42            #+#    #+#             */
-/*   Updated: 2021/02/03 17:47:18 by agiraude         ###   ########.fr       */
+/*   Updated: 2021/02/03 17:51:59 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 size_t	ft_strlen(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, const char *src);
-ssize_t	ft_write(int fd, const void *buf, size_t count); 
+ssize_t	ft_write(int fd, const void *buf, size_t count);
 ssize_t	ft_read(int fd, void *buf, size_t count);
 char	*ft_strdup(const char *s);
 
--- includes/libasm_bonus.h
+++ includes/libasm_bonus.h
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 11:42:21 by user42            #+#    #+#             */
-/*   Updated: 2021/02/03 17:47:42 by agiraude         ###   ########.fr       */
+/*   Updated: 2021/02/03 17:52:40 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-int		ft_atoi_base(const char *s, const char *base);
-void	ft_list_push_front(t_list **list, void *data);
-int		ft_list_size(t_list *list);
-void	ft_list_sort(t_list **begin_list, int (*cmp)());
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void*));
+int					ft_atoi_base(const char *s, const char *base);
+void				ft_list_push_front(t_list **list, void *data);
+int					ft_list_size(t_list *list);
+void				ft_list_sort(t_list **begin_list, int (*cmp)());
+void				ft_list_remove_if
+(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void*));
 
-t_list	*ft_create_elem(void *data);
-void	ft_list_delone(t_list *elem, void (*free_fct)(void*));
+t_list				*ft_create_elem(void *data);
+void				ft_list_delone(t_list *elem, void (*free_fct)(void*));
 
 #endif
